d3f12f62b1578d57d93f6834470577b3247bdce5
unknown <rich10dev@gmail.com>
2024-05-19 17:07:43 +0200

    new

---
+++ api/app.py
@@ -0,0 +1,31 @@
+import warnings
+
+import subprocess
+
+import db
+
+from flask import Flask, jsonify, request, abort
+
+warnings.filterwarnings("ignore")
+
+app = Flask(__name__)
+
+items = []
+
+@app.route('/api/etudiant', methods=['GET'])
+def get_etudiants():
+    db.getetudiants()
+    result = db.resultsExportEtudiants
+    print (result)
+    return jsonify({'item': result}), 201
+
+
+@app.route('/api/etudiant', methods=['POST'])
+def create_etudiant():
+    db.createetudiant(request.json)
+
+    return jsonify({'item': 'etudiant cree'}), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
